d83309d3a37088b96bb18a86d2d054014cece7e1
stlmx <1291100135@qq.com>
2026-04-22 13:44:10 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+/mnt/ssd_data/limingxuan/service/scripts/latex-docker-build.sh \
+--engine pdflatex \
+/mnt/ssd_data/limingxuan/service/power_llava_latex \
+TII-Articles-LaTeX-template/tii-articles-template.tex
